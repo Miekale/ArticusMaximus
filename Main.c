82f03642f162a626b378170af85650be44cac7ce
hepromark <markdo28102004@gmail.com>
2023-06-13 16:44:36 -0400

    Code Skeleton

--- Main.c
+++ Main.c
@@ -1,36 +1,21 @@
-
-void initilizeSensors()
-{
-	SensorType[S1] = sensorEV3_Touch;
-	SensorType[S2] = sensorEV3_Ultrasonic;
-
-	SensorType[S3] = sensorEV3_Color;
-	wait1Msec(50);
-	SensorMode[S3] = modeEV3Color_Color;
-	wait1Msec(50);
-
-	SensorType[S4] = sensorEV3_Gyro;
-	wait1Msec(50);
-	SensorMode[S4] = modeEV3Gyro_Calibration;
-	wait1Msec(100);
-	SensorMode[S4] = modeEV3Gyro_RateAndAngle;
-	wait1Msec(50);
-}
-
-void zero()
-{
-	//move x and y until they hit the touch sensor
-
-	//initilize quardinate 0, 0
-
-}
-
-
-
+// Main Pseudocode
 
 task main()
 {
+	// Initialize Sensors
 
+	// Open File
 
+	// Zero axises
 
+	// ---- DRAWING LOOP ---- //
+	/* while file has instructions
+		if draw function:
+			draw
+		else:
+			move
+
+	*/
+
+	// close file
 }
--- move_draw.c
+++ move_draw.c
@@ -1,7 +1,86 @@
+// Functions required to move and draw robot
+void move_pen(float pos_0, float pos_1);
+void calc_motor_power(float pos_0, float pos_1, int max_power, int motor_powers);
+float calc_angle(float pos_0, float pos_1);
 
+// ---- UNIT TESTS -----
 task main()
 {
+	return;
+}
+
+// Function Definitions
+void move_pen(float pos_0, float pos_1, bool draw=False, int max_draw_power, int max_move_power)
+{
+	/* Controls x motor and y motor to move pen from starting position
+	to ending position. Draw determines if
+
+	PARAMETERS
+	----------
+	pos_0: float array[x,y], starting position.
+	pos_1: float array[x,y], target position.
+	max_draw_power: int <100, maximum motor power allowed while drawing
+	max_move_power: int <100, maximum motor power allowed while moving
+
+	RETURNS
+	-------
+	void
+	*/
+
+	// Determine angle
+
+	// Determine motor power
+
+	// Move motors at power
+
+	// Keep moving motor until limit reached / passed
+
+}
 
 
+void calc_motor_power(float pos_0, float pos_1, int max_power, int motor_powers)
+{
+	/*
+	Calculates x motor and y motor power from starting and ending positions,
+	scaled by max power. Returns by reference
 
+	PARAMETERS
+	----------
+	pos_0: float array[x,y], starting position.
+	pos_1: float array[x,y], target position.
+	max_power: int, positive < 100, maximum power allowed.
+	motor_powers: int array[power x, power y], reference output.
+
+	RETURNS
+	-------
+	motor_powers: int array[power x, power y]
+
+	*/
+
+	// Funny Trig
+
+	return;
+}
+
+float calc_angle(float pos_0, float pos_1)
+{
+	/*
+	Calculates angle (degrees) between two points,
+	relative to [1, 0] vector.
+
+	PARAMETERS
+	----------
+	pos_0: float array[x,y], starting position.
+	pos_1: float array[x,y], target position.
+
+
+	RETURNS
+	-------
+	angle: float degrees between the two points
+
+	*/
+
+	// Funny Trig
+
+	return 0;
 }
