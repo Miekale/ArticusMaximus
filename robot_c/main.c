21eec927c02b78906ccb90fadb9adcf8b7617594
hepromark <markdo28102004@gmail.com>
2023-07-26 17:29:54 -0400

    removed PID Code from main

--- robot_c/main.c
+++ robot_c/main.c
@@ -19,38 +19,6 @@ const string shapeArray[MAX_SHAPES] = {"Square", "Circle"};
 const string fileNames[MAX_FILES] = {"amongus.txt", "goat.txt", "contour_output_3.txt", "contour_output_4.txt"};
 const string shapeNames[MAX_SHAPES] = {"square.txt", "circle.txt"};
 
-// PID Controller
-typedef struct
-{
-	// Controller gains terms
-	float kp;
-	float ki;
-	float kd;
-
-	// Controller motion profile
-	float speed;
-
-	// Derivative low pass filter time constant
-	float tau;
-
-	// Output Limits
-	float lim_min;
-	float lim_max;
-
-	// Sample Time (seconds)
-	float sample_time;
-
-	// Controller stored vars
-	float integrator;
-	float prev_error;
-	float differentiator;
-	float prev_measurement;
-
-	// Controller Output Var
-	float output;
-
-} PID_controller;
-
 // ------ FUNCTION PROTOTYPES ------ //
 
 //UI FUNCTIONS
@@ -58,7 +26,6 @@ bool movePointer(int &pointer, int options);
 void dispMain(int pointer);
 void dispShapes(int pointer);
 void dispFiles(int pointer);
-
 
 // CONVERSION FUNCTIONS
 float degrees_to_mm (int degrees, float gear_radius);
@@ -68,12 +35,6 @@ float rad_to_deg(float rad);
 void pos_mm_to_degree(float* mm_pos, float* deg_pos);
 void pos_degree_to_mm(float* mm_pos, float* deg_pos);
 
-// PID CONTROLLER FUNCTIONS
-void PID_controller_log(PID_controller *pid);
-void PID_Controller_reset(PID_controller *pid);
-float PID_controller_update(PID_controller *pid, float set_point, float measurement);
-void draw_PID(PID_controller* pid_x, PID_controller* pid_y, float* target_pos, bool draw);
-
 // ROBOTC MOVEMENT FUNCTIONS
 void initialize_sensors();
 void get_current_pos(float* mm_pos);
@@ -81,7 +42,8 @@ void zero();
 void move_pen_z(bool move_up);
 float calc_angle(float* pos_0, float* pos_1);
 void calc_motor_power(float angle, int max_power, float* motor_powers);
-void draw_no_PID(float* target_pos, bool draw, int max_draw_power, int max_move_power);
+void draw_or_move(float* target_pos, bool draw, int max_draw_power, int max_move_power);
+void draw_image_from_file(string file_name);
 
 // ------ FUNCTION DEFINITIONS ------ //
 bool movePointer(int &pointer, int options)
@@ -178,80 +140,6 @@ void pos_mm_to_degree(float* mm_pos, float* deg_pos)
 void pos_degree_to_mm(float* mm_pos, float* deg_pos) {
 	mm_pos[0] = degrees_to_mm(deg_pos[0], GEAR_RADIUS_X);
 	mm_pos[1] = degrees_to_mm(deg_pos[1], GEAR_RADIUS_Y);
-}
-
-// Printing PID Controller values
-void PID_controller_log(PID_controller *pid)
-{
-	displayString(5, "integrator %.6f\n", pid->integrator);
-	displayString(10, "prev_error %.6f \n", pid->prev_error);
-	displayString(15, "differentiator %.6f \n", pid->differentiator);
-	displayString(20, "prev_measurement %.6f \n", pid->prev_measurement);
-	displayString(25, "output %.6f \n", pid->output);
-	displayString(30, "\n");
-}
-// Reset PID values
-void PID_Controller_reset(PID_controller *pid)
-{
-	// Reset variables
-	pid->integrator = 0.0;
-	pid->prev_error = 0.0;
-	pid->differentiator = 0.0;
-	pid->prev_measurement = 0.0;
-	pid->output = 0.0;
-}
-// Update PID values
-float PID_controller_update(PID_controller *pid, float set_point, float measurement)
-{
-	// Error Signal
-	float error = set_point - measurement;
-
-	// Proportional
-	float proportional = pid->kp * error;
-
-	// Integral
-	pid->integrator += 0.5 * pid->ki * pid->sample_time * (error + pid->prev_error);
-
-	// Anti-Windup with dynamic integrator clamping
-	float lim_min_int = 0, lim_max_int = 0;
-
-	if (pid->lim_max > proportional)
-	{
-		lim_max_int = pid->lim_max - proportional;
-	}
-	if (pid->lim_min < proportional)
-	{
-		lim_min_int = pid->lim_min - proportional;
-	}
-
-	// Limit integrator value
-	if (pid->integrator < lim_min_int)
-	{
-		pid->integrator = lim_min_int;
-	}
-	if (pid->integrator > lim_max_int)
-	{
-		pid->integrator = lim_min_int;
-	}
-
-	// Derivative (low pass filter)
-	pid->differentiator = (2.0 * pid->kd *(measurement - pid->prev_measurement) + (2.0 * pid->tau - pid->sample_time) * pid->differentiator) / (2.0 * pid->tau + pid->sample_time);
-
-	// Compute output
-	pid->output = proportional + pid->integrator + pid->differentiator;
-	if (pid->output < pid->lim_min)
-	{
-		pid->output  = pid->lim_min;
-	}
-	else if (pid->output > pid->lim_max)
-	{
-		pid->output  = pid->lim_max;
-	}
-
-	pid->prev_error = error;
-	pid->prev_measurement = measurement;
-
-	return pid->output;
 }
 
 // Initialize Touch Sensors
@@ -434,7 +322,7 @@ float calc_angle(float* pos_0, float* pos_1)
 	return angle;
 }
 
-void draw_no_PID(float* target_pos, bool draw, int max_draw_power, int max_move_power)
+void draw_or_move(float* target_pos, bool draw, int max_draw_power, int max_move_power)
 {
 	/* Controls x motor and y motor to move pen from starting position
 	to ending position.
@@ -574,69 +462,7 @@ void draw_no_PID(float* target_pos, bool draw, int max_draw_power, int max_move_
 	return;
 }
 
-void draw_PID(PID_controller* pid_x, PID_controller* pid_y, float* target_pos, bool draw)
-{
-	/* Controls x motor and y motor to move pen from starting position
-	to ending position using PID controller.
-
-	PARAMETERS
-	----------
-	pid_x: PID Controller struct for x-direction motor
-	pid_y: PID Controller struct for y-direction motor
-	target_pos: float array[x,y], target position.
-	max_draw_power: int <100, maximum motor power allowed while drawing
-	max_move_power: int <100, maximum motor power allowed while moving
-
-	RETURNS
-	-------
-	*/
-	// If moving only no PID needed
-	if (!draw)
-	{
-		draw_no_PID(target_pos, false, 80, 80);
-		return;
-	}
-
-	// Pen move until within 0.1mm of actual target
-	float const POS_TOL = 0.2;
-
-	// Initialize starting positions
-	float motor_powers[2] = {0,0};
-	float starting_pos[2] = {0, 0};
-	float current_pos[2] = {0, 0};
-	get_current_pos(current_pos);
-	starting_pos[0] = current_pos[0];
-	starting_pos[1] = current_pos[1];
-
-	// Motion profile
-	float x_f = target_pos[0];
-	float x_i = starting_pos[0];
-	float y_f = target_pos[1];
-	float y_i = starting_pos[1];
-
-	// PID Loop
-	time1[T1] = 0;
-	while ((abs(current_pos[0] - x_f) > POS_TOL) && (abs(current_pos[1] - y_f) > POS_TOL))
-	{
-		// get next point on motion profile
-		float t = time1[T1] * pid_x->speed;
-		float x_t = x_i + (x_f - x_i) * t;
-		float y_t = y_i + (y_f - y_i) * t;
-		// update PID controllers
-		PID_controller_update(pid_x, x_t, current_pos[0]);
-		PID_controller_update(pid_y, y_t, current_pos[1]);
-		motor_powers[0] = pid_x->output;
-		motor_powers[1] = pid_x->output;
-		wait1Msec(pid_x->sample_time);
-		// update current position for loop condition
-		get_current_pos(current_pos);
-	}
-
-	// Turn off motors once target reached
-	motor[motorA] = motor[motorD] = 0;
-}
-
-void draw_image_from_file_no_PID(string file_name)
+void draw_image_from_file(string file_name)
 {
 	int const MAX_DRAW_POWER = 13;
 	int const MAX_MOVE_POWER = 20;
@@ -671,7 +497,7 @@ void draw_image_from_file_no_PID(string file_name)
 		}
 
 		// Move to point
-		draw_no_PID(next_point, is_draw, MAX_DRAW_POWER, MAX_MOVE_POWER);
+		draw_or_move(next_point, is_draw, MAX_DRAW_POWER, MAX_MOVE_POWER);
 		// Output file debug
 		/*
 		writeTextPC(fout, move_or_draw);
@@ -687,91 +513,6 @@ void draw_image_from_file_no_PID(string file_name)
 	move_pen_z(true);
 	zero();
 }
-
-void temp_PID_main(string file_name)
-{
-	/*
-	// Initialize position and zero pen
-	zero();
-
-	// Create controller
-	PID_controller pid_x;
-	PID_controller pid_y;
-	PID_Controller_reset(&pid_x);
-	PID_Controller_reset(&pid_y);
-	writeTextPC(fout, "PID controllers created");
-	writeEndlPC(fout);
-
-	// TODO: Tune Low-pass filter tau and calculate sample time
-	pid_x.sample_time = pid_y.sample_time = 0.005;
-	pid_x.speed = pid_y.speed = 0.2;
-	pid_x.tau = 0.00;
-	pid_x.lim_min = pid_y.lim_min = -80.0;
-	pid_x.lim_max = pid_y.lim_max = 80.0;
-	writeTextPC(fout, "PID values assigned");
-	writeEndlPC(fout);
-
-
-	// TODO: Tune Constants
-	pid_x.kp = 1;
-	pid_x.ki = 0;
-	pid_x.kd = 0;
-	pid_y.kp = 1;
-	pid_y.ki = 0;
-	pid_y.kd = 0;
-	writeTextPC(fout, "PID k-values assigned");
-	writeEndlPC(fout);
-
-	// ---- DRAWING LOOP ---- //
-	// Read each contour
-	string contour_name = "";
-	while (readTextPC(file_name, contour_name))
-	{
-	writeTextPC(fout, "Processing Contour #");
-	writeTextPC(fout, contour_name);
-	writeEndlPC(fout);
-
-	int contour_size = 0;
-	readIntPC(fin, contour_size);
-	for (int point = 0; point < contour_size; point++)
-	{
-	// Determine if D (draw) or M (move)
-	bool is_draw = false;
-	string move_or_draw = "";
-	readTextPC(fin, move_or_draw);
-
-	if (move_or_draw == "D")
-	{
-	is_draw = true;
-	}
-
-	// Get target location
-	float next_point[2] = {0,0};
-	readFloatPC(fin, next_point[0]);
-	readFloatPC(fin, next_point[1]);
-
-	// Move to target location
-	writeTextPC(fout, "Moving to point: ");
-	writeFloatPC(fout, next_point[0]);
-	writeTextPC(fout, " ");
-	writeFloatPC(fout, next_point[1]);
-	writeEndlPC(fout);
-
-	draw_PID(&pid_x, &pid_y, next_point, is_draw);
-	}
-	}
-	// close file
-	closeFilePC(fin);
-	*/
-}
-/*
-task main()
-{
-	openWritePC(fout, "debug_output.txt");
-	string file_name = "contour_output.txt";
-	draw_image_from_file_no_PID(file_name);
-	closeFilePC(fout);
-}*/
 
 task main()
 {
@@ -822,7 +563,7 @@ task main()
 					if(pointer == 1)
 					{
 						string temp_file = fileNames[sub_pointer - 1];
-						draw_image_from_file_no_PID(temp_file);
+						draw_image_from_file(temp_file);
 						displayString(7, "Press enter to return back");
 						while(!getButtonPress(buttonEnter))
 						{}
@@ -831,7 +572,7 @@ task main()
 					else
 					{
 						string temp_file = shapeNames[sub_pointer - 1];
-						draw_image_from_file_no_PID(temp_file);
+						draw_image_from_file(temp_file);
 						displayString(7, "Press enter to return back");
 						while(!getButtonPress(buttonEnter))
 						{}
